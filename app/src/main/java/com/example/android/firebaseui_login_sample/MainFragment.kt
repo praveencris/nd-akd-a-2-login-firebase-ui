aacf3216c6b51fe43c99ab50b28dbdbeed889bef
Praveen <pkchmits@gmail.com>
2021-02-15 11:25:44 +0530

    Successfully added login ui for Google or Email options using firebaseUi Library

--- app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/MainFragment.kt
@@ -17,6 +17,7 @@
 package com.example.android.firebaseui_login_sample
 
 import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -37,7 +38,7 @@ class MainFragment : Fragment() {
 
     companion object {
         const val TAG = "MainFragment"
-        const val SIGN_IN_RESULT_CODE = 1001
+        const val SIGN_IN_REQUEST_CODE = 1001
     }
 
     // Get a reference to the ViewModel scoped to this Fragment
@@ -61,50 +62,76 @@ class MainFragment : Fragment() {
         observeAuthenticationState()
 
         binding.authButton.setOnClickListener {
-            // TODO call launchSignInFlow when authButton is clicked
+            // DONE call launchSignInFlow when authButton is clicked
+            launchSignInFlow()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        // TODO Listen to the result of the sign in process by filter for when
+
+        // DONE Listen to the result of the sign in process by filter for when
         //  SIGN_IN_REQUEST_CODE is passed back. Start by having log statements to know
         //  whether the user has signed in successfully
-    }
-
-    /**
-     * Observes the authentication state and changes the UI accordingly.
-     * If there is a logged in user: (1) show a logout button and (2) display their name.
-     * If there is no logged in user: show a login button
-     */
-    private fun observeAuthenticationState() {
-        val factToDisplay = viewModel.getFactToDisplay(requireContext())
-
-        // TODO Use the authenticationState variable from LoginViewModel to update the UI
-        //  accordingly.
-        //
-        //  TODO If there is a logged-in user, authButton should display Logout. If the
-        //   user is logged in, you can customize the welcome message by utilizing
-        //   getFactWithPersonalition(). I
-
-        // TODO If there is no logged in user, authButton should display Login and launch the sign
-        //  in screen when clicked. There should also be no personalization of the message
-        //  displayed.
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == RESULT_OK) {
+                // User successfully signed in
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
     }
 
 
-    private fun getFactWithPersonalization(fact: String): String {
-        return String.format(
-            resources.getString(
-                R.string.welcome_message_authed,
-                FirebaseAuth.getInstance().currentUser?.displayName,
-                Character.toLowerCase(fact[0]) + fact.substring(1)
-            )
+/**
+ * Observes the authentication state and changes the UI accordingly.
+ * If there is a logged in user: (1) show a logout button and (2) display their name.
+ * If there is no logged in user: show a login button
+ */
+private fun observeAuthenticationState() {
+    val factToDisplay = viewModel.getFactToDisplay(requireContext())
+
+    // TODO Use the authenticationState variable from LoginViewModel to update the UI
+    //  accordingly.
+    //
+    //  TODO If there is a logged-in user, authButton should display Logout. If the
+    //   user is logged in, you can customize the welcome message by utilizing
+    //   getFactWithPersonalition(). I
+
+    // TODO If there is no logged in user, authButton should display Login and launch the sign
+    //  in screen when clicked. There should also be no personalization of the message
+    //  displayed.
+}
+
+
+private fun getFactWithPersonalization(fact: String): String {
+    return String.format(
+        resources.getString(
+            R.string.welcome_message_authed,
+            FirebaseAuth.getInstance().currentUser?.displayName,
+            Character.toLowerCase(fact[0]) + fact.substring(1)
         )
-    }
+    )
+}
 
-    private fun launchSignInFlow() {
-        // TODO Complete this function by allowing users to register and sign in with
-        //  either their email address or Google account.
-    }
+private fun launchSignInFlow() {
+    // DONE Complete this function by allowing users to register and sign in with
+    //  either their email address or Google account.
+
+    val providers = arrayListOf(
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+        AuthUI.IdpConfig.EmailBuilder().build()
+    )
+
+    startActivityForResult(
+        AuthUI.getInstance().createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build(), MainFragment.SIGN_IN_REQUEST_CODE
+    )
+}
 }
